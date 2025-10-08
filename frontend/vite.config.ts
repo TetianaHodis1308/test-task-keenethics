8d8eb4d2066d02bcc6924dcd9b5e60be15d111a3
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-10-08 17:58:19 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/test-task-keenethics/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
